e7a05e4c4c57c50a3c8ce7d56e5935c78ce29a48
Julia-Run <yjdu@link.cuhk.edu.hk>
2022-03-22 12:25:07 +0800

    2022-03-22

--- 76.minimum-window-substring.cpp
+++ 76.minimum-window-substring.cpp
@@ -10,38 +10,52 @@ class Solution
 public:
     string minWindow(string s, string t)
     {
-        // to find mini substring of s which includes all chars in t
-        // substring: lost one char in head, find one in the tail. --- find min
+
+        // substring --> have to be ordered
+        // drop one single element from the start then find from end
+
+        // 1. deal with chars in t;
         vector<bool> flag(128, false);
-        vector<int> charSum(128, 0);
-        // preparation
-        for (int i = 0; i < t.size(); ++i)
+        vector<int> charNumsT(128, 0);
+        for (auto c : t)
         {
-            flag[t[i]] = true;
-            ++charSum[t[i]];
+            flag[c] = true;
+            ++charNumsT[c];
         }
-        // to do the job
-        int countT = 0, lo = 0, start = 0, minSize = s.size() + 1;
+
+        // lo to drop from start, minLo for easy output;
+        // n for if counting how many chars of t are included in sub;
+        int lo = 0, start = 0, minSize = s.size() + 1, n = 0;
+
         for (int hi = 0; hi < s.size(); ++hi)
         {
+            // 02. include all chars of t in 1st sub
             if (flag[s[hi]])
             {
-                if (--charSum[s[hi]] >= 0)
-                    ++countT;
+                if (--charNumsT[s[hi]] >= 0)
+                    // key 01: why have to be >= 0?
+                    // if three are 4 "A" in s, 2 "A" in t, currently iniV = 2;
+                    // A1, v=1,++n; A2,v=0,++n; A3,v=-1; A4,v=-2;
+                    ++n;
             }
-            // when all chars included in s
-            while (countT == t.size())
+
+            // when all chars included
+            while (n == t.size())
             {
-                // refresh minSize
+                // 03. log & refresh current minSize and start
                 if (hi - lo + 1 < minSize)
                 {
-                    start = lo;
                     minSize = hi - lo + 1;
+                    start = lo;
                 }
-                // lose one char in head, break while;
-                if (flag[s[lo]] && ++charSum[s[lo]] > 0)
+                // 04. drop one single element (must be in t) from lo
+                if (flag[s[lo]])
                 {
-                    --countT;
+                    if (++charNumsT[s[lo]] > 0)
+                        --n;
+                    // key 01: iniV = -2; 4A in current sub, 2A in t;
+                    // drop: A1,v=-1; A2, v=0;---2A in sub---;A3,v=1>0,lost 1A, --n;
+                    // the 2nd conditions
                 }
                 ++lo;
             }
